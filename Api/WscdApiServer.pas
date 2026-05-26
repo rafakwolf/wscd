913a5f26e87b3a50c3a1cc2361c975a5318a04c3
Rafael Wolf <rafakwolf@gmail.com>
2026-05-25 22:10:14 -0300

    keep only sqllite + remove unused files

--- Api/WscdApiServer.pas
+++ Api/WscdApiServer.pas
@@ -10,18 +10,11 @@ uses
   mormot.core.os,
   mormot.core.text,
   mormot.core.unicode,
-  mormot.db.core,
-  mormot.db.sql,
-  mormot.db.sql.odbc,
-  mormot.db.raw.sqlite3,
-  mormot.orm.core,
-  mormot.orm.sql,
   mormot.rest.core,
   mormot.rest.http.server,
   mormot.rest.server,
   mormot.rest.sqlite3,
   mormot.db.raw.sqlite3.static,
-  WscdApiConfig,
   Agenda.repository,
   Agenda.controller,
   Recibo.repository,
@@ -33,17 +26,13 @@ type
   TWscdApiServer = class(TRestServerDB)
   private
     FAgendaController: TAgendaEndpointController;
-    FExternalDb: TSqlDBConnectionProperties;
     FReciboController: TReciboEndpointController;
     FVendedorController: TVendedorEndpointController;
-    FInheritedCreateCalled: Boolean;
-    procedure InitializeApi(ACreateMissingTables: Boolean);
+    procedure InitializeApi;
     function RouteApi(Ctxt: TRestServerUriContext): Boolean;
   public
     constructor CreateInMemory(const ARoot: RawUtf8 = 'api'); reintroduce;
     constructor CreateWithDatabase(const ADbFileName: TFileName; const ARoot: RawUtf8 = 'api'); reintroduce;
-    constructor CreateWithFirebirdOdbc(const AConnectionString: RawUtf8;
-      ACreateMissingTables: Boolean = True; const ARoot: RawUtf8 = 'api'); reintroduce;
     destructor Destroy; override;
   published
     procedure Health(Ctxt: TRestServerUriContext);
@@ -51,9 +40,11 @@ type
 
   TWscdApiRunner = class
   private
+    FDbFileName: TFileName;
     FHttpServer: TRestHttpServer;
+    FPort: RawUtf8;
     FServer: TWscdApiServer;
-    FSettings: TWscdApiSettings;
+    class function EnvOrDefault(const AName, ADefault: string): string; static;
     procedure EnsureDatabaseFolder;
   public
     constructor Create;
@@ -64,53 +55,21 @@ type
 
 implementation
 
-function CreateWscdModel(const ARoot: RawUtf8): TOrmModel;
-begin
-  Result := TOrmModel.Create([TOrmAgenda, TOrmRecibo, TOrmVendedor], ARoot);
-end;
-
 constructor TWscdApiServer.CreateInMemory(const ARoot: RawUtf8);
 begin
   inherited CreateWithOwnModel([TOrmAgenda, TOrmRecibo, TOrmVendedor], {HandleUserAuthentication=}False, ARoot);
-  FInheritedCreateCalled := True;
-  InitializeApi({ACreateMissingTables=}True);
+  InitializeApi;
 end;
 
 constructor TWscdApiServer.CreateWithDatabase(const ADbFileName: TFileName; const ARoot: RawUtf8);
 begin
   inherited CreateWithOwnModel([TOrmAgenda, TOrmRecibo, TOrmVendedor], ADbFileName, {HandleUserAuthentication=}False, ARoot);
-  FInheritedCreateCalled := True;
-  InitializeApi({ACreateMissingTables=}True);
+  InitializeApi;
 end;
 
-constructor TWscdApiServer.CreateWithFirebirdOdbc(const AConnectionString: RawUtf8;
-  ACreateMissingTables: Boolean; const ARoot: RawUtf8);
-var
-  Model: TOrmModel;
+procedure TWscdApiServer.InitializeApi;
 begin
-  Model := CreateWscdModel(ARoot);
-  try
-    FExternalDb := TSqlDBOdbcConnectionProperties.Create('', AConnectionString, '', '');
-    FExternalDb.Dbms := dFirebird;
-    OrmMapExternalAll(Model, FExternalDb, [regMapAutoKeywordFields]);
-
-    inherited Create(Model, SQLITE_MEMORY_DATABASE_NAME, {HandleUserAuthentication=}False);
-    FInheritedCreateCalled := True;
-    Model.Owner := Self;
-    InitializeApi(ACreateMissingTables);
-  except
-    FExternalDb.Free;
-    FExternalDb := nil;
-    if Model.Owner = nil then
-      Model.Free;
-    raise;
-  end;
-end;
-
-procedure TWscdApiServer.InitializeApi(ACreateMissingTables: Boolean);
-begin
-  if ACreateMissingTables then
-    Server.CreateMissingTables;
+  Server.CreateMissingTables;
 
   FAgendaController := TAgendaEndpointController.Create(Orm);
   FReciboController := TReciboEndpointController.Create(Orm);
@@ -123,9 +82,7 @@ begin
   FVendedorController.Free;
   FReciboController.Free;
   FAgendaController.Free;
-  if FInheritedCreateCalled then
-    inherited Destroy;
-  FExternalDb.Free;
+  inherited Destroy;
 end;
 
 procedure TWscdApiServer.Health(Ctxt: TRestServerUriContext);
@@ -147,20 +104,25 @@ begin
     Result := True;
 end;
 
+class function TWscdApiRunner.EnvOrDefault(const AName, ADefault: string): string;
+begin
+  Result := GetEnvironmentVariable(AName);
+  if Result = '' then
+    Result := ADefault;
+end;
+
 constructor TWscdApiRunner.Create;
 begin
   inherited Create;
-  FSettings := TWscdApiSettings.Load;
+  FPort := StringToUtf8(EnvOrDefault('PORT', '3001'));
+  FDbFileName := EnvOrDefault('WSCD_DB', 'data/wscd-api.db3');
 end;
 
 procedure TWscdApiRunner.EnsureDatabaseFolder;
 var
   Folder: TFileName;
 begin
-  if FSettings.DatabaseEngine <> wdeSQLite then
-    Exit;
-
-  Folder := ExtractFilePath(FSettings.SQLiteFileName);
+  Folder := ExtractFilePath(FDbFileName);
   if Folder <> '' then
     ForceDirectories(Folder);
 end;
@@ -168,18 +130,10 @@ end;
 procedure TWscdApiRunner.Start;
 begin
   EnsureDatabaseFolder;
-
-  if FSettings.DatabaseEngine = wdeFirebird then
-    FServer := TWscdApiServer.CreateWithFirebirdOdbc(
-      FSettings.OdbcConnectionString, FSettings.CreateMissingTables, 'api')
-  else
-    FServer := TWscdApiServer.CreateWithDatabase(FSettings.SQLiteFileName, 'api');
-
-  FHttpServer := TRestHttpServer.Create(FSettings.HttpPort, [FServer], '+', HTTP_DEFAULT_MODE);
+  FServer := TWscdApiServer.CreateWithDatabase(FDbFileName, 'api');
+  FHttpServer := TRestHttpServer.Create(FPort, [FServer], '+', HTTP_DEFAULT_MODE);
   FHttpServer.AccessControlAllowOrigin := '*';
-  Writeln('WSCDApi running on port ', Utf8ToString(FSettings.HttpPort),
-    ' using ', FSettings.DatabaseDescription);
-  Writeln('Config file: ', FSettings.ConfigFileName);
+  Writeln('WSCDApi running on port ', Utf8ToString(FPort), ' using database ', FDbFileName);
 end;
 
 procedure TWscdApiRunner.Stop;
